a4958e97a0dd6bba7e7b09752e0e658cb700e48e
koji.lin <koji.lin@gmail.com>
2023-06-30 12:51:31 +0900

    Update license header

--- src/main/java/org/jruyi/gradle/thrift/plugin/CompileThrift.java
+++ src/main/java/org/jruyi/gradle/thrift/plugin/CompileThrift.java
@@ -1,4 +1,19 @@
 /*
+ * Copyright 2023 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+/*
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -45,6 +60,8 @@ import java.util.Map;
 import java.util.Set;
 
 public abstract class CompileThrift extends DefaultTask {
+
+    // Forked from https://github.com/jruyi/thrift-gradle-plugin/blob/aef83035ffe141b0507f5a2254aa1f7193976c4a/src/main/groovy/org/jruyi/gradle/thrift/plugin/CompileThrift.groovy
 
     @Incremental
     @InputFiles
--- src/main/java/org/jruyi/gradle/thrift/plugin/ThriftPlugin.java
+++ src/main/java/org/jruyi/gradle/thrift/plugin/ThriftPlugin.java
@@ -1,4 +1,19 @@
 /*
+ * Copyright 2023 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+/*
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -18,6 +33,8 @@ import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 
 public class ThriftPlugin implements Plugin<Project> {
+
+    // Forked from https://github.com/jruyi/thrift-gradle-plugin/blob/aef83035ffe141b0507f5a2254aa1f7193976c4a/src/main/groovy/org/jruyi/gradle/thrift/plugin/ThriftPlugin.groovy
 
     public static final String COMPILE_THRIFT_TASK = "compileThrift";
 
